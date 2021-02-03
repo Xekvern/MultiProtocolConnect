4f3b34b0a51aa53b60225465a9df631925b7f030
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2021-02-03 20:31:06 +0700

    Update Main.php

--- src/xek/MultiProtocolConnect/Main.php
+++ src/xek/MultiProtocolConnect/Main.php
@@ -20,7 +20,7 @@ class Main extends PluginBase implements Listener {
 		$player = $event->getPlayer();
 		$pk = $event->getPacket();
 		if(!$pk instanceof LoginPacket) return;
-			if(ProtocolInfo::CURRENT_PROTOCOL == 419 && == 422){
+			if(ProtocolInfo::CURRENT_PROTOCOL = 419 && = 422){
 				if($pk->protocol >= 418 && $pk->protocol < 424){//pocketmine is not support <418 or >=424 | 418 is rtx beta it support
 					$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;
 				}
