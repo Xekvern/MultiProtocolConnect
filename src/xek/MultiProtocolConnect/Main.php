c71bf8b48915256d772e753dbbdaf306e6dd05ef
Xekyy <77306216+Xekyy@users.noreply.github.com>
2021-02-08 09:16:33 +0800

    Update Main.php

--- src/xek/MultiProtocolConnect/Main.php
+++ src/xek/MultiProtocolConnect/Main.php
@@ -19,16 +19,20 @@ class Main extends PluginBase implements Listener {
 	public function onPacketReceive(DataPacketReceiveEvent $event): void {
 		$player = $event->getPlayer();
 		$pk = $event->getPacket();
-	     if(!$pk instanceof LoginPacket) return;
+		if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 412{
+			if($pk->protocol >= 418 && $pk->protocol < 424){				
+				$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
+		        }
+		}else{
 		     if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 422{
 			     if($pk->protocol >= 418 && $pk->protocol < 424){
-				
+
 				     $pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
 		             }
 		     }else{
-			     
+
 			$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
-		   
+
 		}	
-	}		   
-}	   
+	}	   
+}	
