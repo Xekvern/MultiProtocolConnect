e4152a34092490fbcf1f93425bacec213d2e73d4
Xekyy <77306216+Xekyy@users.noreply.github.com>
2021-02-08 09:20:10 +0800

    Update Main.php

--- src/xek/MultiProtocolConnect/Main.php
+++ src/xek/MultiProtocolConnect/Main.php
@@ -19,20 +19,24 @@ class Main extends PluginBase implements Listener {
 	public function onPacketReceive(DataPacketReceiveEvent $event): void {
 		$player = $event->getPlayer();
 		$pk = $event->getPacket();
-		if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 412{
-			if($pk->protocol >= 418 && $pk->protocol < 424){				
-				$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
-		        }
-		}else{
-		     if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 422{
-			     if($pk->protocol >= 418 && $pk->protocol < 424){
+	        if{$pl instanceof LoginPacket) return;
+		       if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 412{
+			       if($pk->protocol >= 418 && $pk->protocol < 424){	
+				       
+				       $pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
+		                } 
+		        }else{
+			       
+		             if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 422{
+				     
+			             if($pk->protocol >= 418 && $pk->protocol < 424){
 
-				     $pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
-		             }
-		     }else{
+				             $pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
+		                   }
+		           }else{
 
 			$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
-
-		}	
-	}	   
+	             }		
+	       }
+       }	   
 }	
