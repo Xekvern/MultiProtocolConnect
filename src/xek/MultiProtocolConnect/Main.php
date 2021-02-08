917b83aa0e46538d91f9c3cbc86c9f5b7b06d5a0
Xekyy <77306216+Xekyy@users.noreply.github.com>
2021-02-08 09:12:40 +0800

    Update Main.php

--- src/xek/MultiProtocolConnect/Main.php
+++ src/xek/MultiProtocolConnect/Main.php
@@ -23,8 +23,11 @@ class Main extends PluginBase implements Listener {
 		if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 412{
 			if($pk->protocol >= 418 && $pk->protocol < 424){				
 				$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
-		}
-			
+		        }
+		}else{
+			$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;  
+		   
+		}	
 	}
 		   
 }	   
