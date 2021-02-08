3026bc2c6aa66a7691d74d22f277e680e8402af3
Xekyy <77306216+Xekyy@users.noreply.github.com>
2021-02-08 09:42:10 +0800

    Update Main.php

--- src/xek/MultiProtocolConnect/Main.php
+++ src/xek/MultiProtocolConnect/Main.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace xek\MultiProtocolConnect;
@@ -11,25 +10,21 @@ use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener {
-
+	
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-
 	public function onPacketReceive(DataPacketReceiveEvent $event): void {
 		$player = $event->getPlayer();
 		$pk = $event->getPacket();
-	        if{$pl instanceof LoginPacket) return;
-		   if(ProtocolInfo::CURRENT_PROTOCOL = 419 && = 422){
-		       if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 412{
-			       if($pk->protocol >= 418 && $pk->protocol < 424){	     
-				       $pk->protocol = ProtocolInfo::CURRENT_PROTOCOL; 
-			       }
-		        }else{     
-			       $pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;     
-
-		       }
-			       
-	    }
-			  
-}		
+		if(!$pk instanceof LoginPacket) return;
+			if(ProtocolInfo::CURRENT_PROTOCOL = 419 && = 422){
+			if(ProtocolInfo::CURRENT_PROTOCOL == 419 && ProtocolInfo::CURRENT_PROTOCOL == 422){
+				if($pk->protocol >= 418 && $pk->protocol < 424){//pocketmine is not support <418 or >=424 | 418 is rtx beta it support
+					$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;
+				}
+			}else{ 
+				$pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;
+			}
+	     }
+}
